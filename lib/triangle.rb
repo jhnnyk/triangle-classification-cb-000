129029804cd57399ce4257adb43b76488c2816c0
John Knotts <jhnnyk@gmail.com>
2017-02-15 14:24:41 -0700

    complete lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if self.a <= 0 || self.b <= 0 || self.c <= 0
+      raise TriangleError
+    end
+
+    if self.a + self.b <= self.c || self.b + self.c <= self.a || self.a + self.c <= self.b
+      raise TriangleError
+    end
+
+    if self.a == self.b && self.b == self.c
+      :equilateral
+    elsif self.a == self.b || self.b == self.c || self.a == self.c
+      :isosceles
+    elsif self.a != self.b && self.b != self.c && self.a != self.c
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+
 end
